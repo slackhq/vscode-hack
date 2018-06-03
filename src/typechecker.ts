1719370f3269d00d89c44385c1cfe080cb74157c
Pranay Agarwal <pranay.agarwal@gmail.com>
2018-06-03 16:58:20 -0700

    Add syntax highlighting for .hhconfig (as ini), non-LSP mode support for related diagnostic locations

--- src/typechecker.ts
+++ src/typechecker.ts
@@ -23,22 +23,31 @@ export class HackTypeChecker {
 
         const diagnosticMap: Map<vscode.Uri, vscode.Diagnostic[]> = new Map();
         typecheckResult.errors.forEach(error => {
-            let fullMessage = '';
-            let code: number = 0;
-            error.message.forEach(messageUnit => {
-                if (code === 0) {
-                    code = messageUnit.code;
-                }
-                fullMessage = `${fullMessage} ${messageUnit.descr} [${messageUnit.code}]\n`;
-            });
+
             const diagnostic = new vscode.Diagnostic(
                 new vscode.Range(
                     new vscode.Position(error.message[0].line - 1, error.message[0].start - 1),
                     new vscode.Position(error.message[0].line - 1, error.message[0].end)),
-                fullMessage,
+                `${error.message[0].descr} [${error.message[0].code}]`,
                 vscode.DiagnosticSeverity.Error);
-            diagnostic.code = code;
+
+            diagnostic.code = error.message[0].code;
             diagnostic.source = 'Hack';
+
+            const relatedInformation: vscode.DiagnosticRelatedInformation[] = [];
+            for (let i = 1; i < error.message.length; i++) {
+                relatedInformation.push(
+                    new vscode.DiagnosticRelatedInformation(
+                        new vscode.Location(
+                            utils.mapToWorkspaceUri(error.message[i].path),
+                            new vscode.Range(
+                                new vscode.Position(error.message[i].line - 1, error.message[i].start - 1),
+                                new vscode.Position(error.message[i].line - 1, error.message[i].end)
+                            )),
+                        error.message[i].descr));
+            }
+            diagnostic.relatedInformation = relatedInformation;
+
             const file = utils.mapToWorkspaceUri(error.message[0].path);
             const cachedFileDiagnostics = diagnosticMap.get(file);
             if (cachedFileDiagnostics) {
