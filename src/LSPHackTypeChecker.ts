18ae67be7cedf5f7997db2c4dc244b50bbb6de3f
Fred Emmott <fe@fb.com>
2018-07-06 13:09:46 -0700

    Add HHAST support, add error codes, refactor
    These were originally separate diffs, but given they each ended up
    rewriting a substantial portion of the code in the other diffs anyway
    and it's short overall, keeping it as one. This PR
    
    - Adds support for HHAST
      - user/workspace setting for globally enabling/disabling. Off by
      default.
      - user prompting for whether to enable it per-repo
      - that setting is remembered in user settings. workspace settings are
      ignored to avoid allowing project authors to enable prompt-less
      arbitrary code execution when vscode is opened
    - Add error codes to error messages:
      - HHAST: `DontAwaitInALoop: blah blah blah`
      - Hack: `Parsing[1002]: blah blah blah`
    - Refactor:
      - there's now 4 services. Start them more consistently and move stuff
        out from index.ts
      - rename typechecker to LegacyHackTypeChecker

---
+++ src/LSPHackTypeChecker.ts
@@ -0,0 +1,68 @@
+/** @file Integration with the Hack type checker via the LSP */
+
+import * as vscode from 'vscode';
+import { LanguageClient, HandleDiagnosticsSignature } from 'vscode-languageclient/lib/main';
+import * as config from './Config';
+import * as utils from './Utils';
+import * as hack from './types/hack';
+
+export class LSPHackTypeChecker {
+  private context: vscode.ExtensionContext;
+
+  constructor(context: vscode.ExtensionContext) {
+    this.context = context;
+  }
+
+  public async run(): Promise<void> {
+    const context = this.context;
+    const languageClient = new LanguageClient(
+      'Hack Language Server',
+      { command: config.hhClientCommand, args: [...config.hhClientArgs, 'lsp', '--from', 'vscode-hack'] },
+      {
+        documentSelector: [{ language: 'hack', scheme: 'file' }],
+        initializationOptions: { useTextEditAutocomplete: true },
+        uriConverters: { code2Protocol: utils.mapFromWorkspaceUri, protocol2Code: utils.mapToWorkspaceUri },
+        middleware: {
+          handleDiagnostics: this.handleDiagnostics,
+        }
+      },
+    );
+    context.subscriptions.push(languageClient.start());
+  }
+
+  private handleDiagnostics(uri: vscode.Uri, diagnostics: vscode.Diagnostic[], next: HandleDiagnosticsSignature) {
+    next(uri, diagnostics.map(d => {
+      // See https://github.com/facebook/hhvm/blob/028402226993d53d68e17125e0b7c8dd87ea6c17/hphp/hack/src/errors/errors.ml#L174
+      let kind: string;
+      switch (Math.floor((d.code as number) / 1000)) {
+        case 1:
+          kind = 'Parsing';
+          break;
+        case 2:
+          kind = 'Naming';
+          break;
+        case 3:
+          kind = 'NastCheck';
+          break;
+        case 4:
+          kind = 'Typing';
+          break;
+        case 5:
+          kind = 'Lint';
+          break;
+        case 8:
+          kind = 'Init';
+          break;
+        default:
+          kind = 'Other';
+          break;
+      }
+      d.message = kind + '[' + d.code + '] ' + d.message;
+      return d;
+    }));
+  }
+
+  public static isSupported(version: hack.Version): boolean {
+    return version.api_version >= 5;
+  }
+}
