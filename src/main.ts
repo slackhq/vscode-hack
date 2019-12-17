8e2916d9ec9a1f07a32c6d24bb69d3ea3a0e5338
Pranay Agarwal <pranay.agarwal@gmail.com>
2019-12-16 17:24:30 -0800

    Add missing `return`

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ export async function activate(context: vscode.ExtensionContext) {
         }
 
         vscode.window.showErrorMessage(errMsg);
+        return;
     }
 
     const services: Promise<void>[] = [];
