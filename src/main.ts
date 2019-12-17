7fcd218212eedf4fcd995b7eac5290722fff22ea
Yu Chen Hou <houyuchen66@gmail.com>
2019-12-16 16:19:07 -0800

    A more descriptive error message for remote HHVM
    Show a more descriptive error message if the connection to the remote ssh/docker HHVM server fails.

--- src/main.ts
+++ src/main.ts
@@ -15,10 +15,15 @@ export async function activate(context: vscode.ExtensionContext) {
     // check if a compatible verison of hh_client is installed, or show an error message and deactivate extension typecheck & intellisense features
     const version = await hh_client.version();
     if (!version) {
-        vscode.window.showErrorMessage(
-            `Invalid hh_client executable: '${config.clientPath}'. Please ensure that HHVM is correctly installed or configure an alternate hh_client path in workspace settings.`
-        );
-        return;
+        var errMsg = `Invalid hh_client executable: '${config.clientPath}'. Please ensure that HHVM is correctly installed or configure an alternate hh_client path in workspace settings.`;
+
+        if (config.remoteEnabled && config.remoteType === 'ssh') {
+            errMsg = `Unable to connect to remote Hack server, please ensure it is running, and restart VSCode.`;
+        } else if (config.remoteEnabled && config.remoteType === 'docker') {
+            errMsg = `Unable to connect to the HHVM Docker container, please ensure the container and HHVM is running, and restart VSCode.`;
+        }
+
+        vscode.window.showErrorMessage(errMsg);
     }
 
     const services: Promise<void>[] = [];
