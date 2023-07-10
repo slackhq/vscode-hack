ee5d7296039bac6179ec3c09965297353c03e9ee
Pranay Agarwal <pagarwal@slack-corp.com>
2023-07-10 19:17:01 -0400

    Update package names and versions

--- src/debugger.ts
+++ src/debugger.ts
@@ -15,8 +15,8 @@ import * as http from "http";
 import * as net from "net";
 import * as os from "os";
 import { Writable } from "stream";
-import { OutputEvent } from "vscode-debugadapter";
-import { DebugProtocol } from "vscode-debugprotocol";
+import { OutputEvent } from "@vscode/debugadapter";
+import { DebugProtocol } from "@vscode/debugprotocol";
 
 const TWO_CRLF = "\r\n\r\n";
 const CONTENT_LENGTH_PATTERN = new RegExp("Content-Length: (\\d+)");
